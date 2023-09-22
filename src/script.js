07d27a245599c620173d0d52dcbb99d4467112e1
miladesmailpour <milad.esmailpour@gmail.com>
2023-09-21 23:03:02 -0400

    Array Sum solved by using reduce function

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const simpleArraySum = (ar) => {
+  const result = ar.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  return result;
+};
+
+console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));
