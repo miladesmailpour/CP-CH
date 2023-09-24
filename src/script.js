add1b273bbbbac36a2b3089f5cd43737662dab7c
miladesmailpour <milad.esmailpour@gmail.com>
2023-09-23 22:21:01 -0400

    question solved

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const array_1 = [6, 3, 5];
+const array_2 = [7, 3, 9];
+
+const compareTriplets = (a, b) => {
+  let result = [0, 0];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) result[0]++;
+    if (b[i] > a[i]) result[1]++;
+  }
+  return result;
+};
+
+console.log(compareTriplets(array_1, array_2));
